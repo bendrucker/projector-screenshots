e4efab570a2e0fec9ffa346f5fee882599491e57
Ben Drucker <bvdrucker@gmail.com>
2014-10-24 13:22:45 -0400

    Write fake names into slots 1-4 and cycle main name through slot 0 Take and save param-cased screenshots for each iteration

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var Promise   = require('bluebird');
-var wd        = require('wd');
-var asserters = wd.asserters;
-var browser   = wd.promiseChainRemote();
+var Promise    = require('bluebird');
+var changeCase = require('change-case');
+var wd         = require('wd');
+var asserters  = wd.asserters;
+var browser    = wd.promiseChainRemote();
 
 Promise
   .resolve(require(process.env.PLEDGES_PATH))
@@ -12,7 +13,29 @@ Promise
       .init({browserName: 'chrome'})
       .setWindowSize(1440, 900)
       .get(process.env.PROJECTOR_URL)
-      .waitForElementByCss('.loading-indicator', asserters.isNotDisplayed, 5000);
+      .waitForElementByCss('.loading-indicator', asserters.isNotDisplayed, 5000)
+      .safeExecute("document.querySelector('.go-fullscreen').remove()")
+  })
+  .tap(function () {
+    var replacements = ['Allie Kunzig', 'John Fitzpatrick', 'Donna Grogan', 'Diana Gray'];
+    var nodeLookup = "document.querySelectorAll('.donor-list-name')";
+    return Promise.resolve(browser
+      .safeEval(nodeLookup))
+      .then(function (nameNodes) {
+        return [].slice.call(nameNodes, 1, nameNodes.length);
+      })
+      .each(function (node, index) {
+        return browser.safeEval(nodeLookup + '[' + (index + 1) + '].textContent = ' + '"' + replacements[index] + '"');
+      });
+  })
+  .map(function (pledge) {
+    var donor = pledge.donor.name;
+    return browser
+      .safeExecute("document.querySelector('.donor-list-name').textContent = '" + donor + "'")
+      .saveScreenshot('./screenshots/' + changeCase.paramCase(donor) + '.png');
+  },
+  {
+    concurrency: 1
   })
   .bind(browser)
   .finally(browser.quit);
